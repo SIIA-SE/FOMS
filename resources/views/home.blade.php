44172677075ad0a6eff1a7963af9801a598d56a6
sanojan <starnetsweb@gmail.com>
2021-09-22 21:27:18 +0530

    basic layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="my-5 row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">{{ __('Institutes') }}</div>
+
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary">Enter Institute</a>
+                </div>
+            </div>
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+    </div>
+</div>
+@endsection
