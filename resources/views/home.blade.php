33150ed364318216d0078803ffab324c3d586a3c
Sanojan <starnetsweb@gmail.com>
2022-09-18 21:13:07 +0530

    Dashboard trashed count fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
       <div class="d-inline-block mt-4 mr-2 card border-dark" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title font-weight-bold"><i class="bi bi-building"></i> Institutes</h5>
-          <p class="card-text">Total: {{ count(Auth::user()->institutes()->get()) }} | Joined: {{ count(\App\Staff::where('user_id', Auth::user()->id)->get()) }} | Trashed: {{ count(Auth::user()->institutes()->withTrashed()->get()) }}</p>
+          <p class="card-text">Total: {{ count(Auth::user()->institutes()->get()) }} | Joined: {{ count(\App\Staff::where('user_id', Auth::user()->id)->get()) }} | Trashed: {{ count(Auth::user()->institutes()->onlyTrashed()->get()) }}</p>
           <div class="progress">
             <div class="progress-bar" role="progressbar" style="width: {{ count(Auth::user()->institutes()->get()) }}%" aria-valuenow="{{ count(Auth::user()->institutes()->get()) }}" aria-valuemin="0" aria-valuemax="100"></div>
           </div><br />
